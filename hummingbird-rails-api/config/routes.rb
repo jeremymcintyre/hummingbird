6f140f41836191e8c1e60b4d113e000826eeff84
jeremymcintyre & Matthew Villwock <pair+jeremymcintyre.mtvillwock@devbootcamp.com>
2015-02-13 20:09:15 -0800

    Add alias for send_verification_code route to make method name more concise

--- hummingbird-rails-api/config/routes.rb
+++ hummingbird-rails-api/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'user#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+      resources :users do
+        member do
+          get 'missing_phone'
+          post 'send_verification_code', as: "send_verification_code"
+          get 'verify_code'
+          post 'verify_code'
+        end
+      end
   #   resources :products
 
   # Example resource route with options:
