7b8711ff563713c0c18b74edaae34fc476bc1947
William Tsang & Matthew Villwock <pair+kaawang.mtvillwock@devbootcamp.com>
2015-02-15 11:50:15 -0800

    Remove missing_phone route; add DB checks for user.phone_number.nil? and saving user.verification_code,  user.phone_number

--- hummingbird-rails-api/app/controllers/users_controller.rb
+++ hummingbird-rails-api/app/controllers/users_controller.rb
@@ -3,27 +3,28 @@
 class UsersController < ActionController::API
 
   def send_verification_code
+    user = User.find_by(id: params[:id]) # will grab id from front end client
+    user.phone_number = params[:number] # saves their phone number
+    code = generate_verification_code
+    user.verification_code = code # stores generated code in DB
     setup_sms
     send_sms(params[:number],
-      "Your verification code is: " + generate_verification_code)
-    # need to store generated code in DB for checking in verify_code
-  end
-
-  def missing_phone
-    # user = User.find_by(dat google info)
-    # user.phone_number.nil?
-    # redirect user if this returns true
-    # otherwise, return something to client
-    # probably redirect them to send_verification_code client route
+      "Your verification code is: " + code)
   end
 
   def verify_code
-    # code = params[:number]
-    # user = User.find_by(google stuff)
     # this route will be hit when the user sends the verification
     # code they get after requesting it in users#send_verification_code
-    # if code == user.code_verified, redirect to Angular Client messages page
-    # else, re-render verification code page w/ error handling
+    user_entered_code = params[:number] # this is from the client-side form submission
+    user = User.find_by(id: params[:id]) # will grab id from front end client
+    username = User.name
+    if user_entered_code == user.verification_code # compares client and DB codes
+    # if code == user.code_verified, send client welcome message to render
+      render :json => { welcome: "Hey Username #{username}!"}
+    else
+    # else, send client error message to render
+      render :json => {error: "Your verification code is incorrect. Please request another."}
+    end
   end
 
   private
