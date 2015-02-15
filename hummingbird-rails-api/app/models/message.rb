ccd847dac74099e32c44164c412650df9685c0a7
William Tsang & Matthew Villwock <pair+kaawang.mtvillwock@devbootcamp.com>
2015-02-15 15:45:48 -0800

    Add migration column sent boolean, set default false and change model validation from number_to to to

--- hummingbird-rails-api/app/models/message.rb
+++ hummingbird-rails-api/app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :body, :number_to, :send_at_datetime
+  validates_presence_of :body, :to, :send_at_datetime
 end
--- hummingbird-rails-api/db/schema.rb
+++ hummingbird-rails-api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214052436) do
+ActiveRecord::Schema.define(version: 20150215233126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20150214052436) do
     t.string   "to"
     t.string   "body"
     t.datetime "send_at_datetime"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "sent",             default: false
   end
 
   create_table "users", force: :cascade do |t|
