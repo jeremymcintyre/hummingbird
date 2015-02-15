6ac67058e026017b17138687d99438caa3a9b0d8
William Tsang & Matthew Villwock <pair+kaawang.mtvillwock@devbootcamp.com>
2015-02-15 12:52:45 -0800

    add user validation for phone numbers and implement if/elseif conditional for 10 versus 11 digits

--- hummingbird-rails-api/app/controllers/users_controller.rb
+++ hummingbird-rails-api/app/controllers/users_controller.rb
@@ -3,16 +3,36 @@
 class UsersController < ActionController::API
 
   def send_verification_code
-    user = User.find_by(id: params[:id]) # will grab id from front end client
-    user.phone_number = params[:number] # saves their phone number
-    code = generate_verification_code
-    user.verification_code = code # stores generated code in DB
-    setup_sms
-    send_sms(params[:number],
-      "Your verification code is: " + code)
+      user = User.find_by(id: params[:id]) # will grab id from front end client
+      user.phone_number = params[:number] # saves their phone number
+
+    if /\+1\d{10}/.match(params[:number])
+      p "+1 followed by 10 digits"
+      p number
+      code = generate_verification_code
+      user.verification_code = code # stores generated code in DB
+      # setup_sms
+      # send_sms(params[:number],
+      #   "Your verification code is: " + code)
+    elsif /\d{10}/.match(params[:number])
+      number = "+1" + params[:number]
+      code = generate_verification_code
+      user.verification_code = code # stores generated code in DB
+      p "10 digit number with +1 appended"
+      p number
+      # setup_sms
+      # send_sms(params[:number],
+      #   "Your verification code is: " + code)
+    else
+      render :json => {
+        error: "Your phone number format is invalid.
+        Please use this format: +15558675309."}
+    end
   end
 
   def verify_code
+    p "8"*80
+    p params
     # this route will be hit when the user sends the verification
     # code they get after requesting it in users#send_verification_code
     user_entered_code = params[:number] # this is from the client-side form submission
@@ -22,7 +42,6 @@ class UsersController < ActionController::API
     # if code == user.code_verified, send client welcome message to render
       user.phone_verified = true
       render :json => { welcome: "Hey Username #{username}!"}
-
     else
     # else, send client error message to render
       render :json => {error: "Your verification code is incorrect. Please request another."}
