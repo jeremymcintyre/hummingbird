43f29b1b9eebee96b3252d2e637521bae440d0cd
William Tsang & Matthew Villwock <pair+kaawang.mtvillwock@devbootcamp.com>
2015-02-15 18:51:24 -0800

    Add send message batch query for messages with send_at_datetime close to Time.now. Add from field to messages table.

--- hummingbird-rails-api/db/schema.rb
+++ hummingbird-rails-api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215233126) do
+ActiveRecord::Schema.define(version: 20150216003821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150215233126) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.boolean  "sent",             default: false
+    t.string   "from"
   end
 
   create_table "users", force: :cascade do |t|
