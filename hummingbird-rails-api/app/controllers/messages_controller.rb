2e26b52707c9fee8eda3c17cd1cd97dea0b3b2ea
Rex Salisbury <rex.salisbury@gmail.com>
2015-02-17 10:59:14 -0800

    Change message#create so that the body only saves the text, not the number; stitch together the signature immediately before sending out a batch of messages in the message_queue_handler

--- hummingbird-rails-api/app/controllers/messages_controller.rb
+++ hummingbird-rails-api/app/controllers/messages_controller.rb
@@ -6,9 +6,16 @@ class MessagesController < ActionController::API
     user = User.find_by(params[:id])
     from = user.phone_number
     to = params[:number]
-    sms_body = params[:body] + "\n Sent to you from: #{from}"
+    sms_body = params[:body]
     message = user.messages.new(to: to, from: from, body: sms_body, send_at_datetime: params[:send_at_datetime])
     message.save
+  end
+
+  def index
+    messages = User.find_by(params[:id]).messages
+
+    render json: {messages: messages}
+
   end
 
 end
