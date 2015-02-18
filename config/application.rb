b2c8769159bdb0927843554d687ea1922888c8d2
Matthew Villwock <mtvillwock@gmail.com>
2015-02-18 14:50:37 -0800

    remove dotenv gem because it doesn't work with Heroku

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require 'rails/all'
-require 'dotenv'
-Dotenv.load
+
 require "active_record/railtie"
 # require "active_model/railtie"
 # require "action_controller/railtie"
