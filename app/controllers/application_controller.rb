1243e5d824c6f0e4c84fbe9295ef64210859b494
Matthew Villwock <mtvillwock@gmail.com>
2015-03-24 11:47:21 -0700

    remove include Devise line from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::API
   self.responder = ApplicationResponder
   respond_to :html
 
-  include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::RespondWith
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
