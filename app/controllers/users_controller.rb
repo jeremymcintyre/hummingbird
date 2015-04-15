6680f862e89b4f01b6f645ae0cf3f4e0d1922c7d
Matthew Villwock <mtvillwock@gmail.com>
2015-04-15 16:50:35 -0700

    logout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,16 @@ class UsersController < ActionController::API
 
   def index
     # Need to revise this. These routes should not be publicly accessible and/or should return a proper response or warning to anyone accessing them.
-    render json: "APIs don't have index pages"
+    render :json "APIs don't have index pages"
   end
 
   def show
     # Need to revise this. These routes should not be publicly accessible and/or should return a proper response or warning to anyone accessing them.
     user = User.find(1)
     if user.nil?
-      render json: "No users here."
+      render :json "No users here."
     else
-      render json: "Found the user."
+      render :json "Found the user."
     end
   end
 
@@ -22,32 +22,32 @@ class UsersController < ActionController::API
     # if user exists, go to new message page
     # else, make new user, set BCrypt password
     # and return / leave them on login page
-    @user = User.new(
-      email: params[:email],
-      password_hash: params[:password])
+    @user = User.find_or_initialize_by(email: params[:email])
+    @user.update_attributes(password_hash: params[:password])
     @user.password = params[:password]
 
     if @user.save
       session[:user_id] = @user.id
-      p "here's the session"
+      p "here's the session" * 5
       p session
-      p "here's the new user"
+      p "here's the new user" * 5
       p @user
-      render json: { success: "success"}
+      render :json { id: @user.id }
     else
-      render json: { error: "user did not save"}
+      render :json { error: "user did not save"}
     end
   end
 
   def login
     @user = User.find_by(email: params[:email])
     p params
-    p "attempting login with user"
+    p "attempting login with user" * 5
     p @user
+    p session[:user_id]
     if current_user && @user.password == params[:password]
-      render json: { success: "user exists, set to logged in" }
+      render :json { success: "user #{@user.id} exists, and is logged in with session #{session[:user_id]}"}
     else
-      render json: { error: "user not found, login failed"}
+      render :json { error: "user not found, login failed"}
     end
     # if user exists in DB, return JSON
     # telling client to send user to new_message
@@ -57,9 +57,9 @@ class UsersController < ActionController::API
   def logout
     if current_user
       session[:user_id] = nil
-      render json: { success: "success"}
+      render :json { success: "successful logout"}
     else
-      render json: { error: "logout failed"}
+      render :json { error: "logout failed"}
     end
   end
 
--- app/models/concerns/users_helper.rb
+++ app/models/concerns/users_helper.rb
@@ -3,18 +3,18 @@ module UsersHelper
   private
 
   def current_user
-    @user = User.find_by(id: session[:id])
+    @user = User.find_by(id: session[:user_id]) unless session[:used_id].nil?
   end
 
   def set_user_phone
-    current_user.update_attributes(phone_number: params[:number])
+    @user.update_attributes(phone_number: params[:number])
   end
 
   def set_user_verification_code
-    current_user.update_attributes(verification_code: generate_verification_code)
+    @user.update_attributes(verification_code: generate_verification_code)
   end
 
   def set_phone_verified
-    current_user.update_attributes(phone_verified: true)
+    @user.update_attributes(phone_verified: true)
   end
 end
